847d5dc66af50801c74bf746616d278d6d475974
kibadatova-netizen <kibadatova@gmail.com>
2025-11-19 19:19:04 -0600

    added negative Login

--- src/test/java/com/qa/studymate/tests/LoginTest.java
+++ src/test/java/com/qa/studymate/tests/LoginTest.java
@@ -1,9 +1,13 @@
 package com.qa.studymate.tests;
 
 import com.qa.studymate.pages.LoginPage;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.ConfigReader;
+
+import java.time.Duration;
 
 public class LoginTest extends StudymateTestBase{
     @Test
@@ -12,10 +16,22 @@ public class LoginTest extends StudymateTestBase{
         loginPage.loginFunctionality(ConfigReader.readProperty("email"),
                 ConfigReader.readProperty("password"));
         loginPage.clickLoginButton();
-        Thread.sleep(2000);
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.urlContains("/admin"));
+
         Assert.assertEquals(driver.getCurrentUrl(), ConfigReader.readProperty("mainPageUrl"));
     }
 
+    @Test
+    public void negativeLoginFunctionality(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.negativeLoginFunctionality(ConfigReader.readProperty("email"), "asdasda");
 
+        String actualError = loginPage.getErrorMessageText(driver);
+        String expectedError = "Invalid email or password";
+
+        Assert.assertEquals(actualError, expectedError);
+    }
 
 }
